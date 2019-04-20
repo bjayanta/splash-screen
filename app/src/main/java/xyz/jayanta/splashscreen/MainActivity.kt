2e893ead32ad5d14e544adff0651473bb9ce00c3
Jayanta Biswas <bjayanta.me@gmail.com>
2019-04-20 16:44:26 +0600

    first commit

---
+++ app/src/main/java/xyz/jayanta/splashscreen/MainActivity.kt
@@ -0,0 +1,12 @@
+package xyz.jayanta.splashscreen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
